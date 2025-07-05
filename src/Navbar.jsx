2cad5523e8f93c9537d1ae98873a1fd628a79e6d
Engineer-Collo <collinslikhomba@gmail.com>
2025-07-05 16:04:14 +0300

    navbar update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = () => {
   return (
     <header className="bg-white shadow-md relative z-50">
       {/* Top Contact Info */}
-      <div className="bg-gray-400 text-white text-sm py-2 px-4">
-        <div className="max-w-7xl mx-auto flex justify-between items-center gap-2">
+      <div className="bg-gray-400 text-white text-sm py-1 px-4">
+        <div className="max-w-7xl mx-auto flex justify-between items-center gap-1">
           {/* Phone Left */}
           <div className="flex items-center gap-1">
             <FaPhone className="text-white" />
